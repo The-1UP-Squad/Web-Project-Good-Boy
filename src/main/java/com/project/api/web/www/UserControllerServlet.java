eef92af61561686f7c18bf86ba3885150d6c5a4a
2174708 <2174708@LTAU001574.cts.com>
2022-07-21 13:21:05 +0930

    Another commit

--- src/main/java/com/project/api/web/www/UserControllerServlet.java
+++ src/main/java/com/project/api/web/www/UserControllerServlet.java
@@ -1,11 +1,16 @@
 package com.project.api.web.www;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class UserControllerServlet
@@ -13,13 +18,56 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/UserControllerServlet")
 public class UserControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	private UserDbUtil userDbUtil; 
+	
+	@Resource(name="www/Web-Project-Good-Boy")
+	private DataSource dataSource;
+	
+	
+	@Override
+	public void init() throws ServletException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		super.init();
+		
+		// create our user db util .. and pass in the conn pool / data source
+		try {
+			userDbUtil = new UserDbUtil(dataSource);
+		}
+		catch (Exception exc) {
+			throw new ServletException(exc);
+		}
+	}
+
+
+
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		try {
+		// list the users .. in mvc fashion
+		listEmployees(request, response);
+		}
+		catch (Exception exc) {
+			throw new ServletException(exc);
+		}
+		
+	}
+
+
+        private void listEmployees(HttpServletRequest request, HttpServletResponse response) 
+        	throws Exception {
+		// get users from db util
+        	List<Employee> employees = userDbUtil.getEmployees();
+        	
+        // add users to the request
+        	request.setAttribute("EMPLOYEE_LIST", employees);
+        	
+        // send to jsp page
+        	RequestDispatcher dispatcher = request.getRequestDispatcher("/list-employees.jsp");
+        	dispatcher.forward(request, response);
+		
 	}
 
 }
